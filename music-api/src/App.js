87a7f8b8193c5bb7d25237b3913c94f67d406fe1
Rachaelsq <intuitiveharmonymedicine@gmail.com>
2020-12-28 21:44:11 -0600

    router is set up

--- music-api/src/App.js
+++ music-api/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
@@ -19,10 +20,12 @@ function App() {
   return (
     <React.Fragment>
       <Navbar />
-      <ProductList />
-      <Details />
-      <Default />
-      <Cart />
+      <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route exact path="/details" component={Details} />
+          <Route exact path="/cart" component={Cart} />
+          <Route component={Default} />     
+      </Switch>
       <div>  
       <h1>  
 Top Hitz      </h1>  
--- music-api/src/index.js
+++ music-api/src/index.js
@@ -1,33 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import bootstrap from "bootstrap/dist/css/bootstrap.min.css"; 
 
 // import * as serviceWorker from "./serviceWorker";  
 
   
-ReactDOM.render(  
-  <React.StrictMode>  
-    <App />  
-  </React.StrictMode>,  
-  document.getElementById("root")  
-);  
-//
-
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <Router>
+          <App />
+      </Router> 
+  ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-// serviceWorker.unregister();  

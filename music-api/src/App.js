7e50d12f04920816adb225232e0da8265fe55572
Rachaelsq <intuitiveharmonymedicine@gmail.com>
2020-12-28 20:04:37 -0600

    bootstrap is working

--- music-api/src/App.js
+++ music-api/src/App.js
@@ -1,26 +1,56 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstap/dist/css/bootstrap.min.css'
+// import 'bootstap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+
+
+
+
+//mine
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h3>hello forom app</h3>
+      <div className="container py-4">  
+      <h1 className="text-center text-uppercase">  
+        Welcome to react app development  
+      </h1>  
+      <div>  
+        <h3>Bootstrap 4 Buttons</h3>  
+        <button type="button" className="btn btn-primary rounded-0">  
+          Primary  
+        </button>  
+        <button type="button" className="btn btn-secondary rounded-0">  
+          Secondary  
+        </button>  
+        <button type="button" className="btn btn-light rounded-0">  
+          Light  
+        </button>  
+        <button type="button" className="btn btn-danger rounded-0">  
+          Danger  
+        </button>  
+        <button type="button" className="btn btn-success rounded-0">  
+          Success  
+        </button>  
+        <button type="button" className="btn btn-info rounded-0">  
+          info  
+        </button>  
+        <button type="button" className="btn btn-warning rounded-0">  
+          Warning  
+        </button>  
+        <button type="button" className="btn btn-dark rounded-0">  
+          Dark  
+        </button>  
+      </div>  
+    </div>  
+    </React.Fragment>
+    //
+    
+    //
   );
 }
 
